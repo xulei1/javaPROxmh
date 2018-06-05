a6124362b889bb8916454f125b44404279ddde8a
xulei <799873165@qq.com>
2018-06-05 14:57:59 +0800

    add

--- ProXMH/src/com/fatie.java
+++ ProXMH/src/com/fatie.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import com.ldyb.mysqlconnection.SqlConnection;
+
 import com.mysql.jdbc.Statement;
 
 /**
